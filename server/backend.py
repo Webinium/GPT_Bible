62fb18571f99db40e2cc4be7777c9bfda67e2406
Webinium <155104828+Webinium@users.noreply.github.com>
2024-01-03 18:10:05 +0100

    Update backend.py

--- server/backend.py
+++ server/backend.py
@@ -31,8 +31,7 @@ class Backend_Api:
             _conversation = request.json['meta']['content']['conversation']
             prompt = request.json['meta']['content']['parts'][0]
             current_date = datetime.now().strftime("%Y-%m-%d")
-            system_message = 'system_message = '
-Nom de l'IA: WebiChat
+            system_message = 'system_message = 'Nom de l'IA: WebiChat
 Spécialisations: Chrétienté, la Bible, Livres Apocryphes et Pseudépigraphiques
 Capacités linguistiques: Multilingue, avec réponses adaptées à la langue de l'utilisateur
 
@@ -45,9 +44,7 @@ Directives pour l'utilisateur:
 
 Exemple de prompt:
 Utilisateur: "Quelle est l'importance du Livre d'Enoch dans les textes apocryphes ?" (en français)
-WebiChat: "Le Livre d'Enoch est considéré comme important dans les textes apocryphes car il offre une perspective unique sur les anges, la cosmologie, et la fin des temps. Il a influencé diverses traditions religieuses et est souvent étudié pour comprendre les croyances anciennes sur le surnaturel et l'apocalyptique." (en français)
-"""
-'
+WebiChat: "Le Livre d'Enoch est considéré comme important dans les textes apocryphes car il offre une perspective unique sur les anges, la cosmologie, et la fin des temps. Il a influencé diverses traditions religieuses et est souvent étudié pour comprendre les croyances anciennes sur le surnaturel et l'apocalyptique." (en français)'
 
             extra = []
             if internet_access:
